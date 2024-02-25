a24455b4654df83ba4cde2fd11c1a0e5b077fb35
Dwi Satrio W <31796624+dwisatriow@users.noreply.github.com>
2024-02-25 16:50:19 +0700

    feat(ui): update responsive design

--- app/image/[...path]/page.tsx
+++ app/image/[...path]/page.tsx
@@ -16,7 +16,7 @@ export default async function ImagePage({
   return (
     <div className='container flex h-[calc(100vh-85px)] flex-col items-center justify-center'>
       {/* Card container */}
-      <Card className='mx-auto grid h-[360px] w-full max-w-[540px] items-center rounded-lg p-3'>
+      <Card className='mx-auto grid h-[320px] w-full max-w-[540px] items-center rounded-lg p-3 sm:h-[360px]'>
         {/* Image preview */}
         <CardContent className='relative flex h-full flex-col items-center'>
           <Image
